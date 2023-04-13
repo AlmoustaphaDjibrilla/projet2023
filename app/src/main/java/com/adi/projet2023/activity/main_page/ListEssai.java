98e330e26a551997b24ac496473be8d87d74aa5a
AlmoustaphaDjibrilla <115007995+AlmoustaphaDjibrilla@users.noreply.github.com>
2023-04-13 18:52:19 +0200

    Création du modele d'un local

--- app/src/main/java/com/adi/projet2023/activity/main_page/ListEssai.java
+++ app/src/main/java/com/adi/projet2023/activity/main_page/ListEssai.java
@@ -40,6 +40,7 @@ public class ListEssai extends AppCompatActivity {
         locals.add(new Maison("maison", "maison", "maison"));
         locals.add(new Entreprise("enterprise", "niamey", "niamey"));
 /**/
+        //Bonjour
         AdapterLocal adapterLocal= new AdapterLocal(getApplicationContext(), locals);
 
         listEssai.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
